d3a057022b47b2fbfd4a94f151937cf14740d215
youngdong82 <nfs82young@gmail.com>
2023-04-27 20:14:35 +0900

    ADD: add Google Analytics script

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,31 @@ import '@component/styles/globals.scss'
 import type { AppProps } from 'next/app'
 import { DefaultSeo } from 'next-seo'
 import SEO from '../seo.config'
+import Script from 'next/script'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <DefaultSeo {...SEO} />
       <Component {...pageProps} />
+      <Script
+        strategy='afterInteractive'
+        src={`https://www.googletagmanager.com/gtag/js?id=G-KM7KDJ26DJ`} />
+      <Script
+        id='gtag-init'
+        strategy='afterInteractive'
+        dangerouslySetInnerHTML={
+          {
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+            
+              gtag('config', 'G-KM7KDJ26DJ');
+            `
+          }
+        }
+      />
     </>
   )
 }
